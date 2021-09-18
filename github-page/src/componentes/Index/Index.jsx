088bc9003650b946ad92b44fa0f2cd90fe116f76
Emmanuel Rolim Stocco <monitorlab7@unifacef.local>
2021-09-17 22:43:32 -0300

    te

--- github-page/src/componentes/Index/Index.jsx
+++ github-page/src/componentes/Index/Index.jsx
@@ -1,12 +1,12 @@
 import '../MainStyle.css'
 import React from 'react'
-import tvindex from '../images/interrogacao.jpg'
+import interrogacao from '../images/interrogacao.jpg'
 
 export default function Index() {
     return (
         <div className="body">
             <div className="indeximage">
-            <img src={tvindex} alt="Tela de Interrogação"></img>
+            <img src={interrogacao} alt="Tela de Interrogação"></img>
             </div>
         </div>
     )
